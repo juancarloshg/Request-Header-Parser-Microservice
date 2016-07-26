df62b6f4e7a549d89ee900d3c471fd666766e982
Carlos Hernandez <juancarlos_jchg@hotmail.es>
2016-07-26 21:10:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
  	console.log('app listening on port 3000!');
 });
